75e434aea476814fdf89a92b3c441b30167d7259
seth1604abc <seth1605abc@gmail.com>
2024-06-29 15:27:41 +0800

    feat: 建立mysql資料庫連線

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+const { heroConnect } = require('./models/hero/connect')
+
 const envFile = process.env.ENV_FILE;
 if (envFile && envFile !== 'production') {
 	require('dotenv').config({ path: `.env.${envFile}` });
@@ -8,17 +10,23 @@ if (envFile && envFile !== 'production') {
     require('dotenv').config();
 }
 
+// 功能初始化
+(async () => {
+    // 資料庫 [Hero] 連線初始化
+    await heroConnect()
+})()
+
 app.get('/', (req, res) => {
     return res.status(200).send('ok')
 })
 
+// 印出目前使用的PORT以及ENV環境
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-    console.log(); // 換行用
+    console.log();
 	console.log('+' + ''.padEnd(120, '=') + '+');
 	console.log(
 		''.padEnd(20, ' '),
-		// `HOST: [ ${HOST} ] -`,
 		`PORT: [ ${PORT} ] -`,
 		`ENV: [ ${process.env.NODE_ENV} ]`,
 		''.padEnd(20, ' '),
---
+++ models/hero/connect.js
@@ -0,0 +1,39 @@
+const { Sequelize } = require('sequelize');
+
+const envFile = process.env.ENV_FILE;
+if (envFile && envFile !== 'production') {
+	require('dotenv').config({ path: `.env.${envFile}` });
+} else {
+    require('dotenv').config();
+}
+
+const heroSequelize = new Sequelize({
+  dialect: 'mysql',
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  username: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB_HERO,
+  timezone: '+00:00',
+  // logging: 'false',
+  dialectOptions: {
+    ssl: process.env.MYSQL_SSL == 1 ? true : undefined
+  }
+});
+
+// 資料庫連接
+// 若失敗則直接跳出
+async function heroConnect() {
+  try {
+    await heroSequelize.authenticate()
+    console.log('Database [Hero] connection has been established successfully.')
+  } catch (err) {
+    console.log('Database [Hero] connect failed:', err)
+    process.exit(1)
+  }
+}
+
+module.exports = {
+  heroSequelize,
+  heroConnect
+};
